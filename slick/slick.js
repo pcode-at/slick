bae637f3b526094be81b5b53698d10bce666826b
Christoph Pernsteiner <Perni1984@users.noreply.github.com>
2017-09-12 17:26:23 +0200

    try to fix backward sliding
    see #https://github.com/kenwheeler/slick/issues/2002

--- slick/slick.js
+++ slick/slick.js
@@ -1200,8 +1200,8 @@
         if (_.options.infinite === false) {
             max = _.slideCount;
         } else {
-            breakPoint = _.options.slidesToScroll * -1;
-            counter = _.options.slidesToScroll * -1;
+            breakPoint = _.options.slideCount * -1;
+            counter = _.options.slideCount * -1;
             max = _.slideCount * 2;
         }
 
